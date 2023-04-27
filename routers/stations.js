2374028013739a81c2264cb50e33b005d720579c
cr4z4x <anirudh.arcot@proton.me>
2023-04-27 21:06:39 +0530

    filtered stations
    not must also give a .time attribute in format ( hours.minutes ) to find stations only after it

--- routers/stations.js
+++ routers/stations.js
@@ -11,12 +11,25 @@ stationsRouter.get('/stations', async (req, res) => {
 
         const response = await fetch(`https://indian-railway-api.cyclic.app/trains/getRoute?trainNo=${trainNo}`);
         jresponse = await response.json();
-        const stations = jresponse.data.map(obj => ({
+
+
+        const time2 = '1.30';
+        const [hours1, minutes1] = req.body.time.split('.');
+        const [hours2, minutes2] = time2.split('.');
+        const totalMinutes = Number(minutes1) + Number(minutes2);
+        const totalHours = Number(hours1) + Number(hours2) + Math.floor(totalMinutes / 60);
+        const newMinutes = totalMinutes % 60;
+        const newHours = totalHours % 24;
+        const Maxtime = `${newHours}.${newMinutes}`;
+
+        console.log(Maxtime)
+
+        const filteredStations = jresponse.data.map(obj => ({
             source_stn_name: obj.source_stn_name,
             depart: obj.depart
-        }))
+        })).filter(obj => Number(obj.depart) > Number(Maxtime));
 
-        res.status(200).json(stations)
+        res.status(200).json(filteredStations)
 
     } catch (err) {
         console.log(err);
